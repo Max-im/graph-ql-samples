e423745f3f4aee30628ff277dac97e531defe4dc
Maksym Pozhydaiev <m.pozhydaiev@astoundcommerce.com>
2022-06-23 11:16:40 +0300

    remove 06_project

--- 06_project/server/env.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const dotenv = require('dotenv');
-
-const envPath = path.resolve(__dirname, `./.env.${process.env.NODE_ENV}`);
-
-dotenv.config({path: envPath});
--- 06_project/server/src/models/Model.js
+++
@@ -1,9 +0,0 @@
-import db from '../db';
-
-class Model {
-    constructor() {
-        this.db = db;
-    }
-}
-
-module.exports = Model;
